44e29a485a9ff35ee299a91c3baea79530654257
Graham Spencer <gspencer447@yahoo.com>
2023-08-29 11:31:56 -0500

    completed all steps of the exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "science":
+                    Console.WriteLine("Your favorite subject is Science!");
+                    break;
+
+                case "social studies":
+                    Console.WriteLine("Your favorite subject is Social Studies!");
+                    break;
+
+                case "math":
+                    Console.WriteLine("Your favorite subject is Math!");
+                    break;
+
+                case "writing":
+                    Console.WriteLine("Your favorite subject is Writing!");
+                    break;
+
+                case "reading":
+                    Console.WriteLine("Your favorite subject is Reading!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid subject.");
+                    break;
+            }
         }
     }
 }
